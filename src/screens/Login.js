27c5ad2a5af6baa44a6457b6914240434ac74df0
Daryl Lukas <hello@daryllukas.me>
2023-02-23 10:04:49 +0200

    feat: implement auth

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,16 +1,53 @@
 import { StyleSheet, View } from 'react-native';
 import { Button, Input } from '@rneui/themed';
+import { useState } from 'react';
+import { medusaClient } from '../lib/medusa-client';
 
 export default function LoginScreen({ navigation }) {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (fieldName) => (e) => {
+    let newValue = e.nativeEvent.text;
+    setValues({
+      ...values,
+      [fieldName]: newValue,
+    });
+  };
+
+  const handleSubmit = () => {
+    medusaClient.auth
+      .authenticate({
+        email: values.email,
+        password: values.password,
+      })
+      .then(({ customer }) => {
+        console.log(customer.id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <View style={styles.container}>
       <Input
         placeholder="Email"
         keyboardType="email-address"
         style={styles.input}
+        value={values['email']}
+        onChange={handleChange('email')}
       />
-      <Input placeholder="Password" style={styles.input} />
-      <Button size="lg" containerStyle={styles.button} title="Submit" />
+      <Input
+        placeholder="Password"
+        style={styles.input}
+        secureTextEntry
+        value={values['password']}
+        onChange={handleChange('password')}
+      />
+      <Button size="lg" containerStyle={styles.button} title="Submit" onPress={handleSubmit} />
       <Button
         title="I don't have an account"
         type="clear"
